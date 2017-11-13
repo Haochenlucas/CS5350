bae829a06cd4c80ac2b6d61b68adfd8091c80104
haochenz <haochenz@lab3b-19.users.coe.utah.edu>
2017-11-12 17:06:35 -0700

    initialize and parse data

---
+++ Project/parse_data.m
@@ -0,0 +1,13 @@
+function table = parse_data(filename)
+% Author:
+% Haochen Zhang
+% UU
+% Fall 2017
+
+% Columns:
+% year	month	day	hour	2ason	PM_Dongsi	PM_Dongsihuan	PM_Nongzhanguan	PM_US Post	DEWP	HUMI	PRES	TEMP	cbwd	Iws	precipitation	Iprec
+% -1 means missing data
+filename = 'BeijingPM20100101_20151231.csv';
+Beijing_parsed = csvread(filename,1,1);
+end
+
